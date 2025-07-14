c22854cf7edf1a799cbcf1735e8e4132d0ac6e05
JAIDEEP TAAK <jaideeptaak2@gmail.com>
2025-07-14 19:59:06 +0530

    Update program1.cpp

--- oop lab file/program1.cpp
+++ oop lab file/program1.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 using namespace std;
 
-class Student {
+class university_orientation
+{
+private:
+    char student_name[20];
+    int student_id;
+    char branch[10];
+
 public:
-    void showName() {
-        cout << "This is my class." << endl;
+    void insert_data()
+    {
+        cout << "enter student name:-";
+        cin >> student_name;
+
+        cout << "enter student id:-";
+        cin >> student_id;
+
+        cout << "enter branch:-";
+        cin >> branch;
+    }
+
+    void display()
+    {
+        cout << "Student Name: " << student_name << endl;
+        cout << "Student ID: " << student_id << endl;
+        cout << "Branch: " << branch << endl;
     }
 };
 
-int main() {
-    Student s;
-    s.showName();
+int main()
+{
+    university_orientation u1;
+    u1.insert_data();
+    u1.display();
     return 0;
 }
